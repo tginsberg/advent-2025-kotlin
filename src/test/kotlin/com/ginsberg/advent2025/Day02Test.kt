27016cd016f1730c4cb690c36f7d14d0149bc404
Todd Ginsberg <todd@ginsberg.com>
2025-12-03 07:22:26 -0500

    Day 2 - Gift Shop

--- src/test/kotlin/com/ginsberg/advent2025/Day02Test.kt
+++ src/test/kotlin/com/ginsberg/advent2025/Day02Test.kt
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-@DisplayName("Day 1")
+@DisplayName("Day 2")
 class Day02Test {
 
     // Arrange
